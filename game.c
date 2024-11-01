29475f7c64f2e9608ef633148dfd3c951e418c63
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2024-11-01 14:30:04 +0100

    function printBoard

--- game.c
+++ game.c
@@ -5,3 +5,16 @@ void initBoard(char board[3][3]) {
         for (int j = 0; j < 3; j++)
             board[i][j] = ' ';
 }
+
+void printBoard(char board[3][3]) {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < 2) printf("|");
+        }
+        printf("\n");
+        if (i < 2) printf("---|---|---\n");
+    }
+    printf("\n");
+}
