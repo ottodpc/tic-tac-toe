79f77e768a9e9198eab92e16d4a13b94cef9d50c
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2024-11-01 14:31:15 +0100

    function checkWin

--- game.c
+++ game.c
@@ -28,3 +28,23 @@ void playerMove(char board[3][3], char player) {
     } while (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ');
     board[row][col] = player;
 }
+
+int checkWin(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+            return 1;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+            return 1;
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
+        return 1;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
+        return 1;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] == ' ')
+                return -1;
+    
+    return 0; // Égalité
+}
